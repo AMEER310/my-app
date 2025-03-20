686e041d85f9d4f19bc2e122d3cca1516aa6d86c
ameerabdo <ameeralaaabdo2004@gmail.com>
2025-03-20 07:55:22 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import FlightSearch from "./components/FlightSearch";
+import TravelDashboard from "./components/TravelDashboard"
+import "./App.css";
+import PrepareTrip from "./components/PrepareTrip";
+import PopularDestinations from "./components/PopularDestinations";
+import RecommendedHolidays from "./components/RecommendedHolidays";
+import PopularStays from "./components/PopularStays";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Header />
+      <FlightSearch />
+      <TravelDashboard/>
+      <PrepareTrip/>
+      <PopularDestinations/>
+      <RecommendedHolidays/>
+      <PopularStays/>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
